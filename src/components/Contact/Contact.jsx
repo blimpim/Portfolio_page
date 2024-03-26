7bff83626ca8068c343a03170d6bbe20b953071f
Blimpim <blimpim@gmail.com>
2024-03-25 21:11:37 -0700

    Fixed form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,6 @@
+import { useState } from 'react';
 import Media from '../Media/Media';
 import './Contact.scss';
-
-import { useState } from 'react';
 
 const encode = (data) => {
   return Object.keys(data)
@@ -17,30 +16,21 @@ export default function Contact() {
   };
 
   const handleSubmit = async (e) => {
+    e.preventDefault();
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...this.state })
+      body: encode({ "form-name": "contact", ...formData })
     })
-      .then(() => alert("Success!"))
-      .catch(error => alert(error));
-    e.preventDefault();
-
+    .then(() => alert("Your message has been sent!"))
+    .catch(error => alert(error));
   };
 
-  
   return (
     <section>
       <h2>Get in touch</h2>
-
-      <form name="contact" data-netlify="true" data-netlify-honeypot="bot-field" hidden>
-      <input type="text" name="name" />
-      <input type="email" name="email" />
-      <textarea name="message"></textarea>
-    </form>
-
-      <form className="Contact" name="contact" method="post" onSubmit={handleSubmit} >
-      <input type="hidden" name="form-name" value="contact" />
+      <form name="contact" onSubmit={handleSubmit} method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="contact" />
         <div>
           <div className="formItem">
             <label>Name</label>
@@ -51,17 +41,12 @@ export default function Contact() {
             <input type="email" name="email" className="input" placeholder="johndoe@gmail.com" value={formData.email} onChange={handleChange} />
           </div>
         </div>
-
         <div id="formText">
           <label>Message</label>
-          <textarea name="message" type="text" placeholder="Your message here..." value={formData.message} onChange={handleChange}></textarea>
+          <textarea name="message" className="input" placeholder="Your message here..." value={formData.message} onChange={handleChange}></textarea>
         </div>
-
-        
-  
-        <input className="Button" type="submit" value="Send"></input>
+        <input className="Button" type="submit" value="Send" />
       </form>
-
       <Media fill="#A44234" className="_Contact" />
     </section>
   );
